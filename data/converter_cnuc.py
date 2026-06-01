ccfa30008ea8152d8709fadba3eaf8a11a56d4fa
ecs-prog <erissoncameli@gmail.com>
2026-05-31 22:19:25 -0500

    auto: data/converter_cnuc.py

--- data/converter_cnuc.py
+++ data/converter_cnuc.py
@@ -64,7 +64,7 @@ def coords_to_geojson(shape):
         return {"type": "Point", "coordinates": [pts[0][0], pts[0][1]]}
     return None
 
-sf = shapefile.Reader(SHP, encoding='latin-1')
+sf = shapefile.Reader(SHP, encoding='utf-8')
 campos = [f[0] for f in sf.fields[1:]]
 
 features = []
